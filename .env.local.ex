5dc8b25962ca15a7545cfdacf8b692df97dbf1b4
VyHoanq <104727859+VyHoanq@users.noreply.github.com>
2024-11-14 10:15:00 +0700

    Create .env.local.ex

---
+++ .env.local.ex
@@ -0,0 +1,13 @@
+NEXT_PUBLIC_SANITY_DATASET="production"
+NEXT_PUBLIC_SANITY_PROJECT_ID="..."
+NEXT_PUBLIC_BASE_URL="http://localhost:3000"
+
+
+# Sanity Studio to read
+# this is NEED for sanity to see the requested variables in the studio deployment
+SANITY_STUDIO_PROJECT_ID="...."
+SANITY_STUDIO_DATASET="production"
+
+# Clerk
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=...
+CLERK_SECRET_KEY=...
